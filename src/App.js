a8f449599c798768b6369f1efc987feeaa035dcb
Het Vallabhbhai Mendpara <hmendpar@sundevils.asu.edu>
2022-07-18 16:30:24 -0700

    Login Page with authentication functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import './App.css';
-import Bins from './components/bins'
-import barcode from './assets/barcode.svg'
+import React,{useState} from 'react';
+import LoginForm from './components/LoginForm';
+
 function App() {
-  const bins = Array.from({length: 16}, (_, index) => {
-    var label = ""
-    if(index < 4){
-      label = "A" + (index+1).toString()
-    }
-    else if(index < 8){
-      label = "B" + (index - 3).toString()
-    }
-    else if(index < 12){
-      label = "C" + (index - 7).toString()
-    }
-    else{
-      label = "D" + (index - 11).toString()
-    }
-    return <Bins key={index} label={label}/>;
-  });
-  return (
-    <div>
-      <section className='inventory'>
-        <div className='barcode'>
-          <img alt="barcode" src={barcode}></img>
-          <h3>RECEIVING</h3>
+      // Remove this part after making a connection to authentication DB
+      const adminUser = {
+        email: "admin@admin.com",
+        password:"admin"
+      }
+      // 
+      const [user,setUser] = useState({name :"", email: ""});
+      const [error,setError] = useState("");
+
+      const Login = details => {
+        if (details.email == adminUser.email && details.password == adminUser.password){
+          setUser({
+            name: details.name,
+            email: details.email
+          })    
+        } else{
+          setError("Details not found !")
+        }
+      }
+      const Logout = () => {
+        setUser({name: "", email: ""});
+      }
+      return(
+        <div className="App">
+          {(user.email != "") ? (
+            // put a link to a dashborad, and remove the "Welcome" part 
+            <div className="welcome">
+              <h2>Welcome,<span>{user.name}</span></h2>
+              <button onClick={Logout}>Logout</button>
+              </div>
+          ) : ( 
+            <LoginForm Login={Login} error={error}/>
+          )}
+
         </div>
-      </section>
-      <section className='center'>
-        <div className='bins'>
-          {bins}
-        </div>
-        <div className='carts'></div>
-      </section>
-      <section className='record'></section>
-    </div>
-  );
+
+      );
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import './../style/LoginForm.css';
+
+function LoginForm({Login,error}) {
+    const [details, setDetails] = useState({name:"", email:"", password:"",});
+
+    const submitHandler = e =>{
+        e.preventDefault();
+        Login(details);
+
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='box-form'>
+                <div className="left">
+                    <div className="overlay">
+                        <h1>WareHouse Royale</h1>
+                        <span>
+                        <p>Let's Trade!</p>	
+                        </span>
+                    </div>
+                </div>
+        
+                <div className='right'>
+                    <div className="content">
+                        Login
+                    </div>
+
+                    <div className='inputs'>
+                        <input placeholder='username' className='user_name' type="email" name="email" id="email"onChange={e =>setDetails({...details, email: e.target.value})} value={details.email}/>
+                        <input placeholder='password' className='password' type="password" name="password" id="password"onChange={e =>setDetails({...details, password: e.target.value})} value={details.password}/>
+                    </div>
+                    {(error != null) ? (<div className='error'>{error}</div>) : ""}
+                    <div className='button_div'>
+                        <input className='ibutton' type="submit" value="LOGIN"/>
+                    </div>
+
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
