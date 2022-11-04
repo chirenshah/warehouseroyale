36caddd04cc24d267eef54fb52f1e1df3747f62f
hmen9 <hmendpar@asu.edu>
2022-11-05 00:10:44 +0530

    added logout button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import React from "react";
 import { useAuthContext } from './hooks/useAuthContext';
-import LoginForm from "./components/LoginForm";
-import Game from "./components/employee_game";
-// import { HTML5Backend } from "react-dnd-html5-backend";
-// import { DndProvider } from "react-dnd";
-import { emailPasswordAuth } from "./Database/Auth";
-// import { ContextProvider } from "./components/views/Manager/dashboard/contexts/ContextProvider";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import {
-//     Myteam,
-//     Performancemetric,
-//     Recruitmentroom,
-// } from "./components/views/Manager/dashboard/pages";
-// import Messenger from "./components/views/Manager/dashboard/pages/Messenger";
+// Components
+import LoginForm from './components/LoginForm';
 // Views
-import ADashboard from "./views/Admin/ADashboard";
-import MDashboard from "./views/Manager/MDashboard";
-import EDashboard from "./views/Employee/EDashboard";
+import ADashboard from './views/Admin/ADashboard';
+import MDashboard from './views/Manager/MDashboard';
+import EDashboard from './views/Employee/EDashboard';
 
 function App() {
-    const { user, isAuthReady } = useAuthContext();
-  
-    const role = localStorage.getItem('warehouse_user_role');
-  
-    return isAuthReady && user && role === 'admin' ? (
-      <ADashboard />
-    ) : role === 'manager' ? (
-      <MDashboard />
-    ) : role === 'employee' ? (
-      <EDashboard />
-    ) : (
-      <LoginForm />
+  const { user, isAuthReady } = useAuthContext();
 
-    );
-  }
-  
-  export default App;
+  const role = localStorage.getItem('warehouse_user_role');
+
+  return isAuthReady && user && role === 'admin' ? (
+    <ADashboard />
+  ) : role === 'manager' ? (
+    <MDashboard />
+  ) : role === 'employee' ? (
+    <EDashboard />
+  ) : (
+    <LoginForm />
+  );
+}
+
+export default App;
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,23 +1,35 @@
 import { Link } from 'react-router-dom';
+// Hooks
+import useLogout from '../../hooks/useLogout';
+// Components
+import WarehouseSnackbar from '../ui/WarehouseSnackbar';
 // Css
 import './Topbar.css';
 
 export default function Topbar() {
+  const { logout, isPending, error } = useLogout();
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
   return (
     <div className="topbar">
       <Link to="/">
         <span className="topbar__title">Dashboard</span>
       </Link>
-      <Link to="/user">
-        <div className="topbar__user">
-          <img
-            src="https://asu.pure.elsevier.com/files-asset/129976187/tkull.png?w=160&f=webp"
-            alt=""
-            className="topbar__userImage"
-          />{' '}
-          <span className="topbar__username">Hi, Professor</span>
-        </div>
-      </Link>
+      <div className="topbar__user">
+        <img
+          src="https://asu.pure.elsevier.com/files-asset/129976187/tkull.png?w=160&f=webp"
+          alt=""
+          className="topbar__userImage"
+        />{' '}
+        <span className="topbar__username">Hi, Professor</span>
+        <span onClick={handleLogout} className="topbar__logout">
+          Logout
+        </span>
+      </div>
+      {error && <WarehouseSnackbar text={error} />}
     </div>
   );
 }
