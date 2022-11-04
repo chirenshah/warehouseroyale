6a2c6dfe3f5325d3a206b05ac0aaca3d86047752
hmen9 <hmendpar@asu.edu>
2022-11-04 23:39:50 +0530

    fixed login component, fixed other minor things

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,75 +1,77 @@
-import { useState } from 'react';
+import { useState } from "react";
 // Hooks
-import useLogin from '../hooks/useLogin';
+import useLogin from "../hooks/useLogin";
 // Material components
-import Container from '@mui/system/Container';
+import Container from "@mui/system/Container";
 // Components
-import WarehouseSnackbar from './ui/WarehouseSnackbar';
-import WarehouseButton from './ui/WarehouseButton';
+import WarehouseSnackbar from "./ui/WarehouseSnackbar";
+import WarehouseButton from "./ui/WarehouseButton";
 // Css
-import './../style/LoginForm.css';
+import "./../style/LoginForm.css";
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const { login, isPending, error } = useLogin();
+    const { login, isPending, error } = useLogin();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    // TODO: Put validation checks
+        // TODO: Put validation checks
 
-    await login(email, password);
-  };
+        await login(email, password);
+    };
 
-  return (
-    <div className="loginForm">
-      <Container>
-        <form onSubmit={handleSubmit}>
-          <div className="loginForm__box">
-            <div className="loginForm__left">
-              <div className="loginForm__overlay"></div>
-              <div className="loginForm__title">
-                <h1>WareHouse Royale</h1>
-                <h4>Let's Trade!</h4>
-              </div>
-            </div>
+    return (
+        <div className="loginForm">
+            <Container>
+                <form onSubmit={handleSubmit}>
+                    <div className="loginForm__box">
+                        <div className="loginForm__left">
+                            <div className="loginForm__overlay"></div>
+                            <div className="loginForm__title">
+                                <h1>WareHouse Royale</h1>
+                                <h4>Let's Trade!</h4>
+                            </div>
+                        </div>
 
-            <div className="loginForm__right">
-              <h1>Login</h1>
-              <div className="loginForm__inputs">
-                <input
-                  placeholder="Email address"
-                  type="email"
-                  name="email"
-                  id="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                />
-                <input
-                  placeholder="password"
-                  className="password"
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                />
-              </div>
-              {error && <WarehouseSnackbar text={error} />}
-              <WarehouseButton
-                className="loginForm__button"
-                type="submit"
-                text="LOGIN"
-                loading={isPending}
-              />
-            </div>
-          </div>
-        </form>
-      </Container>
-    </div>
-  );
+                        <div className="loginForm__right">
+                            <h1>Login</h1>
+                            <div className="loginForm__inputs">
+                                <input
+                                    placeholder="Email address"
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                                <input
+                                    placeholder="password"
+                                    className="password"
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                    value={password}
+                                />
+                            </div>
+                            {error && <WarehouseSnackbar text={error} />}
+                            <WarehouseButton
+                                className="loginForm__button"
+                                type="submit"
+                                text="LOGIN"
+                                loading={isPending}
+                            />
+                        </div>
+                    </div>
+                </form>
+            </Container>
+        </div>
+    );
 }
 
 export default LoginForm;
