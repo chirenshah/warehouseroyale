9a5d24984d044d2b1ad94c69e118a49fe2f5694c
hmen9 <hmendpar@asu.edu>
2022-11-05 18:58:57 +0530

    added user update functionality

--- src/views/Admin/pages/user/User.jsx
+++ src/views/Admin/pages/user/User.jsx
@@ -1,16 +1,59 @@
-import { Link } from 'react-router-dom';
-// Material components
-import { FiUser, FiCalendar, FiUpload } from 'react-icons/fi';
-import { MdOutlineEmail, MdOutlineLocationOn } from 'react-icons/md';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+// Hooks
+import useDocment from '../../../../hooks/useDocment';
+import { useFirestore } from '../../../../hooks/useFirestore';
+// Material Icons
+import { FiUser, FiUpload } from 'react-icons/fi';
+import { MdOutlineEmail } from 'react-icons/md';
 import { GiSmartphone } from 'react-icons/gi';
 // Components
 import WarehouseHeader from '../../../../components/ui/WarehouseHeader';
 import WarehouseButton from '../../../../components/ui/WarehouseButton';
+import WarehouseCard from '../../../../components/ui/WarehouseCard';
+import WarehouseLoader from '../../../../components/ui/WarehouseLoader';
+import WarehouseSnackbar from '../../../../components/ui/WarehouseSnackbar';
+// Constants
+import { COLLECTION_USERS } from '../../../../utils/constants';
 // Css
 import './User.css';
-import WarehouseCard from '../../../../components/ui/WarehouseCard';
 
 export default function User() {
+  const { id: userId } = useParams();
+
+  const {
+    document: user,
+    isPending,
+    error,
+  } = useDocment(COLLECTION_USERS, userId);
+
+  const { response, updateDocument } = useFirestore();
+
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect called');
+    setFullName(user?.fullName);
+    setUsername(user?.username);
+    setEmail(user?.email);
+    setPhone(user?.phone);
+  }, [user]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Put validation checks
+
+    updateDocument(COLLECTION_USERS, userId, {
+      fullName,
+      username,
+      email,
+      phone,
+    });
+  };
+
   return (
     <div className="user">
       <WarehouseHeader title="Edit user">
@@ -18,104 +61,97 @@ export default function User() {
           <WarehouseButton text="Create new user" />
         </Link>{' '}
       </WarehouseHeader>
-      <div className="userContainer">
-        <WarehouseCard className="userShow">
-          <div className="userShowTop">
-            <img
-              src="https://asu.pure.elsevier.com/files-asset/129976187/tkull.png?w=160&f=webp"
-              alt=""
-              className="userShowImg"
-            />
-            <span className="userShowUsername">
-              Het <br /> Mendpara
-            </span>
-          </div>
-          <div className="userShowBottom">
-            <span className="userShowTitle">Account Details</span>
-            <div className="userShowInfo">
-              <FiUser className="userShowIcon" />
-              <span className="userShowInfoTitle">hmendpar</span>
+      {isPending && <WarehouseLoader />}
+      {error && <WarehouseSnackbar text={error || response.error} />}
+      {user && (
+        <div className="userContainer">
+          <WarehouseCard className="userShow">
+            <div className="userShowTop">
+              <img
+                src={user.avatar || '/assets/anonymous.png'}
+                alt={user.fullName}
+                className="userShowImg"
+              />
+              <span className="userShowUsername">{user.fullName}</span>
             </div>
-            <div className="userShowInfo">
-              <FiCalendar className="userShowIcon" />
-              <span className="userShowInfoTitle">10.12.1999</span>
-            </div>
-            <span className="userShowTitle">Contact Details</span>
-            <div className="userShowInfo">
-              <GiSmartphone className="userShowIcon" />
-              <span className="userShowInfoTitle">+1 123 456 6789</span>
-            </div>
-            <div className="userShowInfo">
-              <MdOutlineEmail className="userShowIcon" />
-              <span className="userShowInfoTitle">hmendpar@gmail.com</span>
-            </div>
-            <div className="userShowInfo">
-              <MdOutlineLocationOn className="userShowIcon" />
-              <span className="userShowInfoTitle">Arizona | USA</span>
-            </div>
-          </div>
-        </WarehouseCard>
-        <WarehouseCard className="userUpdate">
-          <form className="userUpdateForm">
-            <div className="userUpdateLeft">
-              <div className="userUpdateItem">
-                <label>Username</label>
-                <input
-                  type="text"
-                  placeholder="hmendpar"
-                  className="userUpdateInput"
-                />
+            <div className="userShowBottom">
+              <div className="userShowInfo">
+                <FiUser className="userShowIcon" />
+                <span className="userShowInfoTitle">{user.username}</span>
               </div>
-              <div className="userUpdateItem">
-                <label>Full Name</label>
-                <input
-                  type="text"
-                  placeholder="Het Mendpara"
-                  className="userUpdateInput"
-                />
+              <div className="userShowInfo">
+                <GiSmartphone className="userShowIcon" />
+                <span className="userShowInfoTitle">{user.phone}</span>
               </div>
-              <div className="userUpdateItem">
-                <label>Email</label>
-                <input
-                  type="text"
-                  placeholder="het9@gmail.com"
-                  className="userUpdateInput"
-                />
-              </div>
-              <div className="userUpdateItem">
-                <label>Phone</label>
-                <input
-                  type="text"
-                  placeholder="+1 123 456 6789"
-                  className="userUpdateInput"
-                />
-              </div>
-              <div className="userUpdateItem">
-                <label>Address</label>
-                <input
-                  type="text"
-                  placeholder="Arizona State | USA"
-                  className="userUpdateInput"
-                />
+              <div className="userShowInfo">
+                <MdOutlineEmail className="userShowIcon" />
+                <span className="userShowInfoTitle">{user.email}</span>
               </div>
             </div>
-            <div className="userUpdateRight">
-              <div className="userUpdateUpload">
-                <img
-                  className="userUpdateImg"
-                  src="https://asu.pure.elsevier.com/files-asset/129976187/tkull.png?w=160&f=webp"
-                  alt=""
-                />
-                <label htmlFor="file">
-                  <FiUpload className="userUpdateIcon" />
-                </label>
-                <input type="file" id="file" style={{ display: 'none' }} />
+          </WarehouseCard>
+          <WarehouseCard className="userUpdate">
+            <form className="userUpdateForm" onSubmit={handleSubmit}>
+              <div className="userUpdateLeft">
+                <div className="userUpdateItem">
+                  <label>Full Name</label>
+                  <input
+                    type="text"
+                    className="userUpdateInput"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Username</label>
+                  <input
+                    type="text"
+                    className="userUpdateInput"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Email</label>
+                  <input
+                    type="text"
+                    className="userUpdateInput"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="userUpdateItem">
+                  <label>Phone</label>
+                  <input
+                    type="text"
+                    className="userUpdateInput"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
               </div>
-              <WarehouseButton text="Update" success />
-            </div>
-          </form>
-        </WarehouseCard>
-      </div>
+              <div className="userUpdateRight">
+                <div className="userUpdateUpload">
+                  <img
+                    className="userUpdateImg"
+                    src={user.avatar || '/assets/anonymous.png'}
+                    alt={user.fullName}
+                  />
+                  <label htmlFor="file">
+                    <FiUpload className="userUpdateIcon" />
+                  </label>
+                  <input type="file" id="file" style={{ display: 'none' }} />
+                </div>
+                <WarehouseButton
+                  text="Update"
+                  success
+                  type="submit"
+                  loading={response.isPending}
+                />
+              </div>
+            </form>
+          </WarehouseCard>
+        </div>
+      )}
     </div>
   );
 }
