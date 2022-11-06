2514b8e2f990f8d0565931a175d126edc53974bd
hmen9 <hmendpar@asu.edu>
2022-11-06 18:20:14 +0530

    chore:reverted App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React from "react";
 import { useAuthContext } from './hooks/useAuthContext';
-// Components
-import LoginForm from './components/LoginForm';
+import LoginForm from "./components/LoginForm";
+import Game from "./components/employee_game";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+// import { DndProvider } from "react-dnd";
+import { emailPasswordAuth } from "./Database/Auth";
+// import { ContextProvider } from "./components/views/Manager/dashboard/contexts/ContextProvider";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import {
+//     Myteam,
+//     Performancemetric,
+//     Recruitmentroom,
+// } from "./components/views/Manager/dashboard/pages";
+// import Messenger from "./components/views/Manager/dashboard/pages/Messenger";
 // Views
-import ADashboard from './views/Admin/ADashboard';
-import MDashboard from './views/Manager/MDashboard';
-import EDashboard from './views/Employee/EDashboard';
+import ADashboard from "./views/Admin/ADashboard";
+import MDashboard from "./views/Manager/MDashboard";
+import EDashboard from "./views/Employee/EDashboard";
 
 function App() {
-  const { user, isAuthReady } = useAuthContext();
+    const { user, isAuthReady } = useAuthContext();
+  
+    const role = localStorage.getItem('warehouse_user_role');
+  
+    return isAuthReady && user && role === 'admin' ? (
+      <ADashboard />
+    ) : role === 'manager' ? (
+      <MDashboard />
+    ) : role === 'employee' ? (
+      <EDashboard />
+    ) : (
+      <LoginForm />
 
-  const role = localStorage.getItem('warehouse_user_role');
-
-  return isAuthReady && user && role === 'admin' ? (
-    <ADashboard />
-  ) : role === 'manager' ? (
-    <MDashboard />
-  ) : role === 'employee' ? (
-    <EDashboard />
-  ) : (
-    <LoginForm />
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
