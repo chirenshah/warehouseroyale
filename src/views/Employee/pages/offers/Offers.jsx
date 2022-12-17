58eab214ea44bc79bca44435b30bef2245d1ef10
hmen9 <hmendpar@asu.edu>
2022-12-18 00:16:22 +0530

    updated error, loading in Offers

--- src/views/Employee/pages/offers/Offers.jsx
+++ src/views/Employee/pages/offers/Offers.jsx
@@ -1,10 +1,13 @@
 // Hooks
 import { useAuthContext } from '../../../../hooks/useAuthContext';
 import { useDocument } from '../../../../hooks/useDocument';
+import { useFirestore } from '../../../../hooks/useFirestore';
 // Components
 import WarehouseCard from '../../../../components/ui/WarehouseCard';
 import WarehouseHeader from '../../../../components/ui/WarehouseHeader';
 import WarehouseButton from '../../../../components/ui/WarehouseButton';
+import WarehouseLoader from '../../../../components/ui/WarehouseLoader';
+import WarehouseSnackbar from '../../../../components/ui/WarehouseSnackbar';
 // Firestore services
 import {
   acceptOffer,
@@ -18,6 +21,8 @@ import './Offers.css';
 export default function Offers() {
   const { user, updateUser } = useAuthContext();
 
+  const { response, callFirebaseService } = useFirestore();
+
   const {
     document: employee,
     isPending: isEmployeePending,
@@ -25,54 +30,73 @@ export default function Offers() {
   } = useDocument(COLLECTION_USERS, user?.email);
 
   const handleAcceptOffer = async (offer) => {
-    await acceptOffer(user, offer);
+    await callFirebaseService(acceptOffer(user, offer));
 
     updateUser({ ...user, ...offer });
   };
 
   const handleDeclineOffer = async (offer) => {
-    await declineOffer(employee.email, offer.teamId, offer);
+    await callFirebaseService(
+      declineOffer(employee.email, offer.teamId, offer)
+    );
   };
 
   return (
     <div className="offers">
+      {(employeeError || response.error) && (
+        <WarehouseSnackbar text={employeeError || response.error} />
+      )}
       {/* ------------------------------ Active offers ------------------------------ */}
       <WarehouseHeader title="Active offers" />
       <WarehouseCard>
-        <div className="offers__activeOffer">
-          <div>
-            {employee?.offers?.map(({ teamId, share }, index) => (
-              <div key={index}>
-                <h3>
-                  {index + 1}. You are offered a position at Team {teamId} for{' '}
-                  {share}% share in organization
-                </h3>
-                <div className="offers__acceptDecline">
-                  <WarehouseButton
-                    onClick={() => handleAcceptOffer({ teamId, share })}
-                    text="Accept"
-                    success
-                  />
-                  <WarehouseButton
-                    onClick={() => handleDeclineOffer({ teamId, share })}
-                    text="Decline"
-                    warning
-                  />
+        {isEmployeePending ? (
+          <WarehouseLoader />
+        ) : (
+          <div className="offers__activeOffer">
+            {!employee?.offers?.length ? (
+              <h4>No active offers</h4>
+            ) : (
+              employee?.offers?.map(({ teamId, share }, index) => (
+                <div key={index}>
+                  <h3>
+                    {index + 1}. You are offered a position at Team {teamId} for{' '}
+                    {share}% share in organization
+                  </h3>
+                  <div className="offers__acceptDecline">
+                    <WarehouseButton
+                      onClick={() => handleAcceptOffer({ teamId, share })}
+                      text="Accept"
+                      success
+                      loading={response.isPending}
+                    />
+                    <WarehouseButton
+                      onClick={() => handleDeclineOffer({ teamId, share })}
+                      text="Decline"
+                      warning
+                      loading={response.isPending}
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
-        </div>
+        )}
       </WarehouseCard>
 
       {/* ------------------------------ Past offers ------------------------------ */}
       <WarehouseHeader title="Past offers" my />
       <WarehouseCard>
-        {employee?.pastOffers?.map(({ teamId, share }, index) => (
-          <h3 key={index}>
-            {index + 1}. At Team {teamId} for {share}% share
-          </h3>
-        ))}
+        {isEmployeePending ? (
+          <WarehouseLoader />
+        ) : !employee?.pastOffers?.length ? (
+          <h4>No past offers</h4>
+        ) : (
+          employee?.pastOffers?.map(({ teamId, share }, index) => (
+            <h3 key={index}>
+              {index + 1}. At Team {teamId} for {share}% share
+            </h3>
+          ))
+        )}
       </WarehouseCard>
     </div>
   );
