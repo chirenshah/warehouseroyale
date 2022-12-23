684e226e4b1e474045a118e275a93581ec5dc5de
hmen9 <hmendpar@asu.edu>
2022-12-23 22:35:32 +0530

    added classId while creating user

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 // Firestore collections
 export const COLLECTION_USERS = 'users';
 export const COLLECTION_TEAMS = 'teams';
+export const COLLECTION_CLASSES = 'classList';
 export const COLLECTION_CHATS = 'chats';
