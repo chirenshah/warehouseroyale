7c1dde36a1e69accd0da0e606c15f486162690e7
hmen9 <hmendpar@asu.edu>
2023-01-15 17:37:29 +0530

    added excel validations

--- src/utils/functions/parseExcel.js
+++ src/utils/functions/parseExcel.js
@@ -1,9 +1,15 @@
 import { read, utils } from 'xlsx';
+import { isExcelValid } from './isExcelValid';
 
 export const parseExcel = async (file) => {
-  const f = await file.arrayBuffer();
-  const wb = read(f);
-  const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+  try {
+    const f = await file.arrayBuffer();
+    const wb = read(f);
+    const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
 
-  return data;
+    if (isExcelValid(data)) return data;
+  } catch (error) {
+    console.error('Error: ', error);
+    throw error;
+  }
 };
